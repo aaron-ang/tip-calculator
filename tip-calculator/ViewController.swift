3c5d864009d8338cbddf5c42e66e374a68121dd6
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-08-22 18:28:35 +0800

    add optional features

--- tip-calculator/ViewController.swift
+++ tip-calculator/ViewController.swift
@@ -17,17 +17,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+        // Add listener to bill TextField to update total on edit
+        billAmountTextField.addTarget(self, action: #selector(calculateTip(_:)), for: .editingChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Retrieve and populate UserDefaults stored values
+        let lastDatetime: NSDate? = UserDefaults.standard.object(forKey: "datetime") as? NSDate
+        if (lastDatetime != nil) && lastDatetime!.timeIntervalSinceNow >= -600 {
+            billAmountTextField.text = UserDefaults.standard.string(forKey: "billAmt")
+            tipControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: "segmentIndex")
+        }
+        calculateTip(self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        billAmountTextField.becomeFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        billAmountTextField.resignFirstResponder()
     }
 
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
         
         let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        let segmentIndex = tipControl.selectedSegmentIndex
+        let tip: Double = bill * tipPercentages[segmentIndex]
+        let total: Double = bill + tip
         
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        UserDefaults.standard.set(bill, forKey: "billAmt")
+        UserDefaults.standard.set(segmentIndex, forKey: "segmentIndex")
+        UserDefaults.standard.set(NSDate(), forKey: "datetime")
+        
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        
+        tipAmountLabel.text = currencyFormatter.string(from: tip as NSNumber)
+        totalLabel.text = currencyFormatter.string(from: total as NSNumber)
     }
     
 }
